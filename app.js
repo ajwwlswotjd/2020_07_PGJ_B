c4f858d8828c2c9811df3d7be98324b448a6b35f
ajwwlswotjd <ajwwlswotjd@naver.com>
2020-07-09 13:50:13 +0900

    점심시간끝

--- app.js
+++ app.js
@@ -20,15 +20,38 @@ class App {
 	}
 
 	init(){
-		this.productList.forEach(x=>{
-			let div = this.itemTemp(x);
-			document.querySelector(".item-list").appendChild(div);
-			$(div).draggable({
-				cursor:"pointer",
-				helper:"clone",
-				appendTo:".basket",
-				revert:true
+		this.drawList(this.productList);
+
+		document.querySelector("#search-input").addEventListener("input",(e)=>{
+			let keyword = e.target.value;
+			if(keyword.trim() == ""){
+				this.drawList(this.productList);
+				return;
+			}
+			let list = [];
+			this.productList.forEach(x=>{
+				if(search(keyword,x.name).length != 0 || search(keyword,x.brand).length != 0){
+					let name = [];
+					let len = keyword.length;
+					let indexes = search(keyword,x.name);
+					name.push(x.name.substring(0,indexes[0]));
+					indexes.forEach((index,i)=>{
+						name.push("<span class='highlite'>"+x.name.substring(indexes[i],indexes[i]+len)+"</span>");
+						name.push(x.name.substring(indexes[i]+len,indexes[i+1]));
+					});
+					x.high = name.join('');
+					list.push(x);
+				}
 			});
+			if(list.length == 0){
+				document.querySelector(".item-list").innerHTML = "";
+				let h1 = document.createElement("h1");
+				h1.innerHTML = "일치하는 상품이 없습니다.";
+				h1.classList.add("no-exist");
+				document.querySelector(".item-list").appendChild(h1);
+				return;	
+			}
+			this.drawList(list,true);
 		});
 
 		$(".basket").droppable({
@@ -41,14 +64,17 @@ class App {
 					return;
 				}
 				let item = this.productList.find(x=> x.id == drag.dataset.id);
+				item.cnt = 1;
 				let li = this.basketItemTemp(item);
 				li.querySelectorAll(".basket-cnt > button").forEach(x=>{
 					x.addEventListener("click",(c)=>{
 						let num = x.dataset.num;
 						item.cnt += num*1;
 						if(item.cnt <= 1) item.cnt = 1;
-						li.querySelector(".basket-total").innerHTML = `${(item.cnt*item.priceNum).toLocaleString()}원`;
+						item.total = item.cnt*item.priceNum*1;
+						li.querySelector(".basket-total").innerHTML = `${item.total.toLocaleString()}원`;
 						li.querySelector(".basket-cnt > span").innerHTML = item.cnt;
+						this.calcTotal();
 					});
 				});
 				li.querySelector(".basket-close").addEventListener("click",(c)=>{
@@ -56,30 +82,61 @@ class App {
 					this.basket.forEach((x,idx)=>{
 						if(x.id == item.id) index = idx;
 					});
-					log(index);
 					this.basket.splice(index,1);
-					log(this.basket);
 					$(li).remove();
+					this.calcTotal();
 				});
 				document.querySelector(".screen-right-top").prepend(li);
 				this.basket.push(item);
+				this.calcTotal();
 			}
+		});
+
+		$(document).on("click",".popup-close",(e)=>{
+			let
+		});
+
+		document.querySelector("#buy_btn").addEventListener("click",(e)=>{
+			$(".pc-popup").fadeIn();
 		});
 	}
 
-	itemTemp(item){
+	drawList(items,high=false){
+		document.querySelector(".item-list").innerHTML = "";
+		items.forEach(x=>{
+			let div = this.itemTemp(x,high);
+			document.querySelector(".item-list").appendChild(div);
+			$(div).draggable({
+				cursor:"pointer",
+				helper:"clone",
+				appendTo:".basket",
+				revert:true
+			});
+		});
+	}
+
+	calcTotal(){
+		let total = 0;
+		this.basket.forEach(x=>{
+			total += x.total;
+		});
+		document.querySelector(".purchase-total > span").innerHTML = total.toLocaleString();
+		return total;
+	}
+
+	itemTemp(item,high = false){
 		let div = document.createElement("div");
 		div.classList.add("item");
 		div.dataset.id = item.id;
 		div.innerHTML = `
 						<div class="item-brand">
-							${item.brand}
+							${high ? item.high : item.brand}
 						</div>
 
 						<img src="${item.photo}" alt="" class="item-img">
 						
 						<div class="item-name">
-							${item.name}
+							${high ? item.high : item.brand}
 						</div>
 						<div class="item-price">
 							${item.price}원
@@ -91,6 +148,7 @@ class App {
 	basketItemTemp(item){
 		let li = document.createElement("li");
 		li.classList.add("basket-item");
+		item.total = item.cnt*item.priceNum;
 		li.innerHTML = `
 				<img src="${item.photo}" alt="">
 					<div class="basket-item-text">
@@ -128,6 +186,7 @@ class Product {
 		this.priceNum = price.replace(/,/gi,"")*1;
 		this.cnt = 1;
 		this.total = this.priceNum;
+		this.high = "";
 	}
 }
 
@@ -162,7 +221,6 @@ function match(keyword,data){
 function search(keyword,data){
 	let dataCho = cho(data);
 	let indexes = match(keyword,data);
-	log(indexes);
 	let keywordLength = keyword.length;
 	let result = [];
 
